7501d35ebf64f2830adb60527ac5c5699f0f7418
Nilavo <iem.nilavo2001@gmail.com>
2023-02-19 18:56:12 +0530

    set the initial directory structure and routes files

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+async function main() {
+    await mongoose.connect(`mongodb://localhost/votesAPI`);
+    console.log("Connected to Database :: MongoDB");
+}
+main().catch((err) => {
+    console.log("failed to connect ", err);
+});
+
+module.exports = mongoose;
